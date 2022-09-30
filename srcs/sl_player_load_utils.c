0472aa2c78c8de2a9d522eb4803a1db6f4e511b6
ntan-wan <wangtanngan@gmail.com>
2022-09-30 16:58:14 +0800

    updated player_load_utils

--- srcs/sl_player_load_utils.c
+++ srcs/sl_player_load_utils.c
@@ -6,7 +6,7 @@
 /*   By: ntan-wan <ntan-wan@42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 16:30:18 by ntan-wan          #+#    #+#             */
-/*   Updated: 2022/09/30 16:51:58 by ntan-wan         ###   ########.fr       */
+/*   Updated: 2022/09/30 16:57:58 by ntan-wan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,33 @@ void	sl_player_load_imgs_idle(void *mlx, t_img **imgs)
 
 void	sl_player_load_imgs_move(void *mlx, t_img **imgs)
 {
-	sl_load_img(mlx, imgs, "k_right0", "sprite/knight/run_right0.xpm");
-	sl_load_img(mlx, imgs, "k_right1", "sprite/knight/run_right1.xpm");
-	sl_load_img(mlx, imgs, "k_right2", "sprite/knight/run_right2.xpm");
-	sl_load_img(mlx, imgs, "k_right3", "sprite/knight/run_right3.xpm");
-	sl_load_img(mlx, imgs, "k_right4", "sprite/knight/run_right4.xpm");
-	sl_load_img(mlx, imgs, "k_right5", "sprite/knight/run_right5.xpm");
-	sl_load_img(mlx, imgs, "k_left0", "sprite/knight/run_left0.xpm");
-	sl_load_img(mlx, imgs, "k_left1", "sprite/knight/run_left1.xpm");
-	sl_load_img(mlx, imgs, "k_left2", "sprite/knight/run_left2.xpm");
-	sl_load_img(mlx, imgs, "k_left3", "sprite/knight/run_left3.xpm");
-	sl_load_img(mlx, imgs, "k_left4", "sprite/knight/run_left4.xpm");
-	sl_load_img(mlx, imgs, "k_left5", "sprite/knight/run_left5.xpm");
+	sl_load_img(mlx, imgs, "k_run0_r", "sprite/knight/run_right0.xpm");
+	sl_load_img(mlx, imgs, "k_run1_r", "sprite/knight/run_right1.xpm");
+	sl_load_img(mlx, imgs, "k_run2_r", "sprite/knight/run_right2.xpm");
+	sl_load_img(mlx, imgs, "k_run3_r", "sprite/knight/run_right3.xpm");
+	sl_load_img(mlx, imgs, "k_run4_r", "sprite/knight/run_right4.xpm");
+	sl_load_img(mlx, imgs, "k_run5_r", "sprite/knight/run_right5.xpm");
+	sl_load_img(mlx, imgs, "k_run0_l", "sprite/knight/run_left0.xpm");
+	sl_load_img(mlx, imgs, "k_run1_l", "sprite/knight/run_left1.xpm");
+	sl_load_img(mlx, imgs, "k_run2_l", "sprite/knight/run_left2.xpm");
+	sl_load_img(mlx, imgs, "k_run3_l", "sprite/knight/run_left3.xpm");
+	sl_load_img(mlx, imgs, "k_run4_l", "sprite/knight/run_left4.xpm");
+	sl_load_img(mlx, imgs, "k_run5_l", "sprite/knight/run_left5.xpm");
+
+    /*sl_load_img(mlx, imgs, "h_idle0", "sprite/hero/idle0.xpm");
+	sl_load_img(mlx, imgs, "h_idle1", "sprite/hero/idle1.xpm");
+	sl_load_img(mlx, imgs, "h_idle2", "sprite/hero/idle2.xpm");
+	sl_load_img(mlx, imgs, "h_idle3", "sprite/hero/idle3.xpm");
+	sl_load_img(mlx, imgs, "h_run0", "sprite/hero/run0.xpm");
+	sl_load_img(mlx, imgs, "h_run1", "sprite/hero/run1.xpm");
+	sl_load_img(mlx, imgs, "h_run2", "sprite/hero/run2.xpm");
+	sl_load_img(mlx, imgs, "h_run3", "sprite/hero/run3.xpm");
+	sl_load_img(mlx, imgs, "h_run4", "sprite/hero/run4.xpm");
+	sl_load_img(mlx, imgs, "h_run5", "sprite/hero/run5.xpm");
+	sl_load_img(mlx, imgs, "h_attack0", "sprite/hero/attack0.xpm");
+	sl_load_img(mlx, imgs, "h_attack1", "sprite/hero/attack1.xpm");
+	sl_load_img(mlx, imgs, "h_attack2", "sprite/hero/attack2.xpm");
+	sl_load_img(mlx, imgs, "h_attack3", "sprite/hero/attack3.xpm");*/
 }
 
 void	sl_player_load_anim_idle(t_player *player, t_img *imgs)
@@ -62,16 +77,31 @@ void	sl_player_load_anim_idle(t_player *player, t_img *imgs)
 
 void	sl_player_load_anim_move(t_player *player, t_img *imgs)
 {
-	sl_anim_add_frame(player->move_right, sl_img_search("k_right0", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("k_right1", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("k_right2", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("k_right3", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("k_right4", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("k_right5", imgs));
-	sl_anim_add_frame(player->move_left, sl_img_search("k_left0", imgs));
-	sl_anim_add_frame(player->move_left, sl_img_search("k_left1", imgs));
-	sl_anim_add_frame(player->move_left, sl_img_search("k_left2", imgs));
-	sl_anim_add_frame(player->move_left, sl_img_search("k_left3", imgs));
-	sl_anim_add_frame(player->move_left, sl_img_search("k_left4", imgs));
-	sl_anim_add_frame(player->move_left, sl_img_search("k_left5", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("k_run0_r", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("k_run1_r", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("k_run2_r", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("k_run3_r", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("k_run4_r", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("k_run5_r", imgs));
+	sl_anim_add_frame(player->move_left, sl_img_search("k_run0_l", imgs));
+	sl_anim_add_frame(player->move_left, sl_img_search("k_run1_l", imgs));
+	sl_anim_add_frame(player->move_left, sl_img_search("k_run2_l", imgs));
+	sl_anim_add_frame(player->move_left, sl_img_search("k_run3_l", imgs));
+	sl_anim_add_frame(player->move_left, sl_img_search("k_run4_l", imgs));
+	sl_anim_add_frame(player->move_left, sl_img_search("k_run5_l", imgs));
+
+    /*sl_anim_add_frame(player->idle_right, sl_img_search("h_idle0", imgs));
+	sl_anim_add_frame(player->idle_right, sl_img_search("h_idle1", imgs));
+	sl_anim_add_frame(player->idle_right, sl_img_search("h_idle2", imgs));
+	sl_anim_add_frame(player->idle_right, sl_img_search("h_idle3", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("h_run0", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("h_run1", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("h_run2", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("h_run3", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("h_run4", imgs));
+	sl_anim_add_frame(player->move_right, sl_img_search("h_run5", imgs));
+	sl_anim_add_frame(player->attack_right, sl_img_search("h_attack0", imgs));
+	sl_anim_add_frame(player->attack_right, sl_img_search("h_attack1", imgs));
+	sl_anim_add_frame(player->attack_right, sl_img_search("h_attack2", imgs));
+	sl_anim_add_frame(player->attack_right, sl_img_search("h_attack3", imgs));
 }
--- srcs/sl_player_utils.c
+++ srcs/sl_player_utils.c
@@ -6,48 +6,11 @@
 /*   By: ntan-wan <ntan-wan@42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 08:48:45 by ntan-wan          #+#    #+#             */
-/*   Updated: 2022/09/30 16:35:06 by ntan-wan         ###   ########.fr       */
+/*   Updated: 2022/09/30 16:54:43 by ntan-wan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
-
-/*static void	sl_player_load_imgs(void *mlx, t_img **imgs)
-{
-	sl_load_img(mlx, imgs, "h_idle0", "sprite/hero/idle0.xpm");
-	sl_load_img(mlx, imgs, "h_idle1", "sprite/hero/idle1.xpm");
-	sl_load_img(mlx, imgs, "h_idle2", "sprite/hero/idle2.xpm");
-	sl_load_img(mlx, imgs, "h_idle3", "sprite/hero/idle3.xpm");
-	sl_load_img(mlx, imgs, "h_run0", "sprite/hero/run0.xpm");
-	sl_load_img(mlx, imgs, "h_run1", "sprite/hero/run1.xpm");
-	sl_load_img(mlx, imgs, "h_run2", "sprite/hero/run2.xpm");
-	sl_load_img(mlx, imgs, "h_run3", "sprite/hero/run3.xpm");
-	sl_load_img(mlx, imgs, "h_run4", "sprite/hero/run4.xpm");
-	sl_load_img(mlx, imgs, "h_run5", "sprite/hero/run5.xpm");
-	sl_load_img(mlx, imgs, "h_attack0", "sprite/hero/attack0.xpm");
-	sl_load_img(mlx, imgs, "h_attack1", "sprite/hero/attack1.xpm");
-	sl_load_img(mlx, imgs, "h_attack2", "sprite/hero/attack2.xpm");
-	sl_load_img(mlx, imgs, "h_attack3", "sprite/hero/attack3.xpm");
-}
-
-static void	sl_player_load_anim(t_player *player, t_img *imgs)
-{
-	
-	/*sl_anim_add_frame(player->idle_right, sl_img_search("h_idle0", imgs));
-	sl_anim_add_frame(player->idle_right, sl_img_search("h_idle1", imgs));
-	sl_anim_add_frame(player->idle_right, sl_img_search("h_idle2", imgs));
-	sl_anim_add_frame(player->idle_right, sl_img_search("h_idle3", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("h_run0", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("h_run1", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("h_run2", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("h_run3", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("h_run4", imgs));
-	sl_anim_add_frame(player->move_right, sl_img_search("h_run5", imgs));
-	sl_anim_add_frame(player->attack_right, sl_img_search("h_attack0", imgs));
-	sl_anim_add_frame(player->attack_right, sl_img_search("h_attack1", imgs));
-	sl_anim_add_frame(player->attack_right, sl_img_search("h_attack2", imgs));
-	sl_anim_add_frame(player->attack_right, sl_img_search("h_attack3", imgs));
-}*/
 
 void	sl_player_init(void *mlx, t_player **player, t_img **imgs)
 {
